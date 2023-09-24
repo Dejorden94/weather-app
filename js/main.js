b4105e037c07cb3a395bd042d9b1fcaf03336771
Dejorden <d.moerman@hotmail.nl>
2023-09-24 18:10:20 +0200

    fetch json data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("Doet het");
+fetch("http://api.weatherapi.com/v1/current.json?key=a6e57a8225de486bacd153636232409&q=Amsterdam)").then(
+    response => response.json()
+).then(data => {
+    setData(data);
+    return data;
+});
+
+const setData = (data) => {
+
+}
