4d3050195c1f996729ed43a79d66707071f6169a
Dejorden <d.moerman@hotmail.nl>
2023-09-24 18:58:01 +0200

    add weather condition text and icon

--- js/main.js
+++ js/main.js
@@ -7,10 +7,17 @@ fetch("http://api.weatherapi.com/v1/current.json?key=a6e57a8225de486bacd15363623
 
 let place = document.getElementById("js--weather-place");
 let tempc = document.getElementById("js--weather-temp");
+let condi = document.getElementById("js--weather-condtion");
+let icon = document.getElementById("js--weather-icon");
+
+let humi = document.getElementById("js--weather-humidity");
 
 const setData = (data) => {
-    console.log(place);
     place.innerText = data.location.name;
     tempc.innerText = data.current.temp_c + "°";
+    condi.innerText = data.current.condition.text;
 
+    icon.setAttribute("src", data.current.condition.icon);
+
+    humi.innerText = data.current.humidity;
 }
